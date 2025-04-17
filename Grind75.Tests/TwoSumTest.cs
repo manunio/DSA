c39abd33b29856b0b75b6c83ff4d33239352dcf6
manu <max.manu.nair@gmail.com>
2025-04-17 23:31:50 +0530

    Attempting leetcode

---
+++ Grind75.Tests/TwoSumTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Grind75.Tests;
+
+public static class TwoSumTestData
+{
+    public static IEnumerable<object[]> Testcases =>
+        [
+            [new int[] { 2, 7, 11, 15 }, 9, new int[] {0, 1}],
+            [new int[] { 3 , 2 , 4 }, 6 ,new int[] {1, 2}],
+            [new int[] {1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 }, 11, new int[] { 5, 11 } ]
+        ];
+}
+
+public sealed class TwoSumTest
+{
+
+    [Theory]
+    [MemberData(nameof(TwoSumTestData.Testcases), MemberType = typeof(TwoSumTestData))]
+    public void TestBruteForceApproach(int[] nums, int target, int[] expected)
+    {
+        int[] result = TwoSum.BruteForceApproach(nums, target);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(TwoSumTestData.Testcases), MemberType = typeof(TwoSumTestData))]
+    public void TestHashApproach(int[] nums, int target, int[] expected)
+    {
+        int[] result = TwoSum.HashMapApproach(nums, target);
+        Assert.Equal(expected, result);
+    }
+}
---
+++ Grind75/TwoSum.cs
@@ -0,0 +1,39 @@
+﻿namespace Grind75;
+
+public static class TwoSum
+{
+    public static int[] BruteForceApproach(int[] nums, int target)
+    {
+        // 3, 2, 4 = 6
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+
+        return [];
+    }
+
+    public static int[] HashMapApproach(int[] nums, int target)
+    {
+        // value : index
+        Dictionary<int, int> hm = new(nums.Length);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (hm.TryGetValue(diff, out int index)) // found if true
+            {
+                return [index, i];
+            }
+            hm.TryAdd(nums[i], i);
+        }
+
+        return [];
+    }
+}
